22e2aab3bfab42fa254859ed86699cdfa3e73e3a
el7777 <leociffer@gmail.com>
2021-03-31 16:02:14 +0700

    node.js

---
+++ asynchronus.js
@@ -0,0 +1,20 @@
+//asynchronus
+
+const getUserSync = (id, callback) => {
+    const time = id === 1 ? 3000 : 2000
+    setTimeout(() => {
+        const nama = id === 1 ? 'Leo' : 'Alex'
+        callback({ id, nama })
+    }, time)
+}
+
+const userSatu = getUserSync(1, (hasil) => {
+    console.log(hasil)
+})
+
+const userDua = getUserSync(2, (hasil) => {
+    console.log(hasil)
+})
+
+const halo = 'hallo world'
+console.log(halo)
